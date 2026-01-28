57b0a06eb3be9ba6f4fabd0e117277e92911cfd2
Banaseer K A <banaseer123@gmail.com>
2026-01-28 10:12:10 +0530

    Add files via upload

---
+++ preloaded tickets.py
@@ -0,0 +1,13 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+>>> 
+>>> ticket_data = {    'Ticket_No': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+...     'Customer_Name': [ 'Ravi', 'Meera', 'Sam', 'Anu', 'Rakesh', 'Divya', 'Arjun', 'Kiran', 'Leela', 'Nisha'
+...     ],   
+...  'Issue_Description': [
+...         ' Internet not working!!! ', 'slow response, very poor service ',
+...         'GREAT support! issue resolved.',' okay... need help ', 'not BAD but slow', 'Excellent guidance, Very Helpful!', 'good support and good behaviour!', 'Poor handling of technical issue', 'Satisfied. Could be better.',  'Good service... quick response.'
+...     ],  
+...   'Priority': ['High', 'Low', 'High', 'Medium', 'Low', 'High', 'Medium', 'High', 'Low', 'Medium']}
+... print(ticket_data)
